185fb91ed5150451e605310233f4675af298f873
Péter Petrovics <peterpetrovics1@gmail.com>
2021-04-14 17:53:22 +0200

    Alertek létrehozása, kezelése

--- src/main/java/hu/unideb/inf/MainApp.java
+++ src/main/java/hu/unideb/inf/MainApp.java
@@ -1,23 +1,38 @@
 package hu.unideb.inf;
 
+import hu.unideb.inf.model.Registration;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.h2.tools.Server;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/RegistrationScene.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Semmi");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    
+    public void start2(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/Scene.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Semmi");
         stage.setScene(scene);
         stage.show();
     }
@@ -31,7 +46,26 @@ public class MainApp extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            startDatabase();
+        } catch (SQLException ex) {
+            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
         launch(args);
+        stopDatabase();
     }
 
+    private static Server s = new Server();
+    
+    private static void startDatabase() throws SQLException {
+        s.runTool("-tcp", "-web", "-ifNotExists");
+        
+            
+    }
+
+    private static void stopDatabase()  {
+        s.shutdown();
+    }
+    
 }
