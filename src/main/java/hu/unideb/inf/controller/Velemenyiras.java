c5920a7d61c5ba14644a94e8207498f4e16eaca9
Péter Petrovics <peterpetrovics1@gmail.com>
2021-05-02 20:31:34 +0200

    Grafikus felület fejlesztése, Edzői Profil adatbázis létrehozása és adatok adatbázisba vitele

--- src/main/java/hu/unideb/inf/controller/Velemenyiras.java
+++ src/main/java/hu/unideb/inf/controller/Velemenyiras.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
 
 public class Velemenyiras {
@@ -24,9 +25,16 @@ public class Velemenyiras {
     @FXML
     private Rating edzorating;
 
+    
     @FXML
-    void mentesVelemenyiras(ActionEvent event) {
-
+    private Button mentesVelemenyiras;
+    
+    
+    
+    @FXML
+    void mentesVelemenyirasAction(ActionEvent event) {
+        Stage stage2 = (Stage) mentesVelemenyiras.getScene().getWindow();
+        stage2.close();
     }
 
 }
